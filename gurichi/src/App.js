df5f498560ddaf968b78e814e5f547746eaaad5f
Shimsim <timesea96@naver.com>
2022-06-19 13:18:55 +0000

    doing update

--- gurichi/src/App.js
+++ gurichi/src/App.js
@@ -6,14 +6,16 @@ import './App.scss'
 function App() {
 	const newDate = new Date().toLocaleString();
 	
+	let [mode, setMode] = useState(false);
 	let [matchResult, setMatchResult] = useState(['구리치 3:0', '구리치 5:1', '구리치 0:3'])
 	let [addLike, setAddLike] = useState([0,0,0])
 	let [modal, setModal] = useState(false)
+	let [updateModal, setUpdateModal] = useState(false)
 	let [title, setTitle] = useState(0)
 	let [inputValue, setInputValue] = useState('')
 	let [boardTime, setBoardTime] = useState(['1월경기', '2월경기', '3월경기'])
 	
-	const submit = () => {
+	const addList = () => {
 		if (!inputValue) return
 			let copy = [...matchResult]
 					copy.push(inputValue)
@@ -26,6 +28,7 @@ function App() {
 				setBoardTime(copyBoardTime)
 	}
 	
+
   return (
 	<>
 		<div className="App">
@@ -48,6 +51,7 @@ function App() {
 										setAddLike(copy)
 									}}>🖐</span> {addLike[i]} </h3>
 							<h4>{ boardTime[i] }</h4>
+							<div className="click__btn">
 							<button onClick={()=> {
 									let copy = [...matchResult]
 									copy.splice(i,1)
@@ -55,7 +59,9 @@ function App() {
 									addLikeCopy.splice(i,1)
 									setAddLike(addLikeCopy)
 									setMatchResult(copy)
-								}}>X</button>
+								}}>❌</button>
+							<button onClick={()=>{setUpdateModal(!updateModal)}}>수정하기</button>
+							</div>
 							<p>5월 15일 경기</p>
 						</div>
 					)
@@ -64,10 +70,16 @@ function App() {
 			<input type="text" onChange={(e)=> {
 					setInputValue(e.target.value)
 				}}/>
-			<button onClick={ submit }>경기결과 추가하기</button>
+			<button onClick={ addList }>경기결과 추가하기</button>
+			{
+				updateModal == true ?
+					<Update matchResult={matchResult} title={title} setUpdateModal={setUpdateModal}
+				updateModal={updateModal} setMode={setMode} mode={mode}/> : null
+			}
 			
 			{
-				modal == true ? <Modal setMatchResult={setMatchResult} setModal={setModal} modal={modal} matchResult={matchResult} title={title}/> : null
+				modal == true ? <Modal setModal={setModal} modal={modal} matchResult={matchResult} 
+				title={title}/> : null
 			}
 			
 		</div>
@@ -77,17 +89,35 @@ function App() {
 
 const Modal = (props) => {
 	return(
-		<div onClick={()=>{props.setModal(!props.modal)}} className="modal">
+		<div className="modal">
 			<div className="modal__overlay"></div>
 				<div className="modal__content">
 					<h4>{props.matchResult[props.title]}</h4>
 					<p>날짜</p>
 					<p>상세내용</p>
+					<p><button onClick={()=>{props.setModal(!props.modal)}} className="modal__close">❌</button></p>
 				</div>
 		</div>
 	)
 }
 
+function Update(props) {
+		return(
+			<div className="update">
+			<div className="update__overlay"></div>
+				<div className="update__content">
+					<h4>{props.matchResult[props.title]}</h4>
+					<p>상세내용 추가예정</p>
+					<p><button onClick={()=>{
+								props.setUpdateModal(!props.updateModal)
+								props.setMode(!props.mode)
+							}
+						}	
+				  className="update__close">❌</button></p>
+				</div>
+		</div>
+		)
+	}
 export default App;
 
 
