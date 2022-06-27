82cf0de3a1cdd4cd835b38246a8be68364e9576b
Shimsim <timesea96@naver.com>
2022-06-27 11:19:56 +0000

    Fix: input 상태 관리

--- gurichi/src/components/MatchAdd.js
+++ gurichi/src/components/MatchAdd.js
@@ -7,8 +7,6 @@ import { TiTrash, TiPencil } from "react-icons/ti";
 import './MatchAdd.css'
 
 
-
-//나중에 input 창을 하나 더 만들어서 경기 입력창 + 경기결과를 추가해도 될듯
 const MatchAdd = ({
 	onAddToggle,
 	onAddMatch,
@@ -16,36 +14,49 @@ const MatchAdd = ({
 	onRemove,
 	onUpdate
 }) => {
-	const [value, setValue] = useState('')
-	const [mvpValue, setMvpValue] = useState('')
-	const [matchResult, setMatchResult] = useState('')
+	
+	//value, setValue
+	const [data, setdata] = useState({
+		text: '',
+		mvp: '',
+		result: ''
+	})
+	
+	const { text, mvp, result} = data;
+	
+	
+	// const [mvpValue, setMvpValue] = useState('')
+	// const [matchResult, setMatchResult] = useState('')
+	
+
 	
 	const onChange = (e) => {
-		setValue(e.target.value)
+		const { value, name} = e.target
+		setdata({
+			...data, [name]: value
+		})
 	}
 	
-	const onChangeMvp = (e) => {
-		setMvpValue(e.target.value)
-	}
+	// const onChangeMvp = (e) => {
+	// 	setMvpValue(e.target.value)
+	// }
 	
-	const onChangeResult = (e) => {
-		setMatchResult(e.target.value)
-	}
+	// const onChangeResult = (e) => {
+	// 	setMatchResult(e.target.value)
+	// }
 	
 	
 	const onSubmit = (e) => {
 		e.preventDefault();
-		onAddMatch(value, matchResult, mvpValue);
-		setValue('');
+		onAddMatch(text, result, mvp);
+		setdata('');
 		onAddToggle();
 	}
 	
 	//setValue를 변경한다는 것은 즉, value의 값이 변경된다는 것이고, 그러면 form이 열렸을 때 value의 값은 해당 ele의 값이다.
 	useEffect(() => {
 		if(selectedMatch) {
-			setValue(selectedMatch.text)
-			setMvpValue(selectedMatch.mvp)
-			setMatchResult(selectedMatch.result)
+			setdata(selectedMatch)
 		}
 	},[selectedMatch])
 
@@ -57,30 +68,33 @@ const MatchAdd = ({
 				selectedMatch ? 
 					(e) => {
 					e.preventDefault()
-					onUpdate(selectedMatch.id, value, matchResult, mvpValue)
+					onUpdate(selectedMatch.id, text, result, mvp)
 				}
 			: onSubmit
 		}>
 			
 			<input
+				name="text"
 				placeholder="경기를 입력하세요"
-				value={value}
+				value={text}
 				onChange={onChange}
 			/>
 			<input
+				name="mvp"
 				placeholder="Mvp를 입력하세요"
-				value={mvpValue}
-				onChange={onChangeMvp}
+				value={mvp}
+				onChange={onChange}
 			/>
 			<input
+				name="result"
 				placeholder="경기결과를 입력하세요"
-				value={matchResult}
-				onChange={onChangeResult}
+				value={result}
+				onChange={onChange}
 			/>
 			
 			{selectedMatch ? (
 				<div className="rewrite">
-					<TiPencil onClick={()=>{onUpdate(selectedMatch.id, value, matchResult, mvpValue)}}/>
+					<TiPencil onClick={()=>{onUpdate(selectedMatch.id, text, result, mvp)}}/>
 					<TiTrash onClick={()=>{onRemove(selectedMatch.id)}}/>
 				</div>) :
 			
